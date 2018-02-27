2e398a81139227685bc5115b3b9c415073ed3644
Konrad Dryniewicz <s00160273@mail.itsligo.ie>
2018-02-27 23:48:29 +0000

    Code implemented

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if (gender == "female")
+	    if ((age >= 18) && (age <= 30))
+                premium = 5.0f;
+            else
+        if (age >= 31)
+                premium = 2.50f;
+            else
+                premium = 0.0f;
+            else
+	    if (gender == "male")
+	    if ((age >= 18) && (age <= 35))
+                premium = 6.0f;
+            else
+           if (age >= 36)
+                premium = 5.0f;
+            else
+                premium = 0.0f;
+            else
+		premium = 0.0f;
+
+            if (age >= 50)
+                premium = premium * 0.15f;
+            return premium;
+        }
+
+    }
+}
